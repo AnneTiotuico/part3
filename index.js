8c97cb96cd5ab0446f33509a5c5948f8e6d85a2c
Anne Tiotuico <amtiotuico@gmail.com>
2022-07-16 22:09:58 -0700

    finished ex 3.15-3.18 added error handler middleware, made sure put request and /info routes were working

--- index.js
+++ index.js
@@ -25,22 +25,31 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+app.get('/api/persons/info', (request, response) => {
+  Person.find({}).then(persons => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
                 <p>${new Date()}</p>`)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  }).catch(error => {
-    response.status(404).end()
   })
 })
 
-app.delete('/api/persons/:id', async (request, response) => {
-    await Person.findByIdAndDelete(request.params.id)
-    response.status(204).end()
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+        .then(person => {
+          if (person) {
+            response.json(person)
+          } else {
+            response.status(404).end()
+          }
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+          response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // const validEntry = (name, number) => {
@@ -61,7 +70,6 @@ app.post('/api/persons', (request, response) => {
   }
 
   const person = new Person({
-    // id: generateId(),
     name: name,
     number: number
   })
@@ -70,6 +78,33 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+          response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
